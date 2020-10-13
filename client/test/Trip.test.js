1a53d2e79767fda91806a63071318a277e08a9b7
Sean Munoz <smunoz1337@gmail.com>
2020-10-13 13:53:37 -0600

    Added test cases to trip.test bring coverage to 73%

--- client/test/Trip.test.js
+++ client/test/Trip.test.js
@@ -2,6 +2,9 @@ import './jestConfig/enzyme.config.js';
 import React from "react";
 import {shallow} from 'enzyme';
 import Trip from "../src/components/Trip/Trip";
+import {Map} from 'react-leaflet'; //Dont delete
+import 'leaflet/dist/leaflet.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function initialMyClass() {
     const expected = shallow(<Trip/>);
@@ -50,3 +53,69 @@ function initialIndex() {
 }
 
 test("Initial State Index", initialIndex);
+
+function toggleButtonColorTest(){
+    const trip = shallow(<Trip/>);
+    let color = trip.instance().toggleButtonColor()
+    expect(color).toEqual("danger");
+}
+test("ToggleButtonColor", toggleButtonColorTest)
+
+function testAddATrip(){
+    const trip = shallow(<Trip/>)
+    let initial = trip.state().trips.length
+    expect(initial).toEqual(0);
+    trip.instance().addATrip();
+    initial = trip.state().trips.length
+    expect(initial).toEqual(1);
+    trip.instance().addATrip();
+    expect(trip.state().trips[1]).toEqual([])
+}
+
+test("TestAddTrip", testAddATrip)
+
+function testRenderPlaceList(){
+    const trip = shallow(<Trip/>)
+    trip.setState({tripPlaces: [L.latLng(0,0), L.latLng(0,0), L.latLng(0,0)]})
+    trip.instance().renderSearchList()
+    trip.instance().renderTripList()
+    expect(trip.state().tripPlaces.length).toEqual(3)
+}
+
+test("TestRenderPlaceList", testRenderPlaceList)
+
+function testRemoveAPlace(){
+    const trip = shallow(<Trip/>)
+    trip.setState({tripPlaces: [L.latLng(0,0), L.latLng(0,0), L.latLng(0,0)]})
+    expect(trip.state().tripPlaces.length).toEqual(3);
+    trip.instance().removeAPlace();
+    expect(trip.state().tripPlaces.length).toEqual(2)
+}
+
+test("TestRemoveAPlace", testRemoveAPlace)
+
+function testRemoveATrip(){
+    const trip = shallow(<Trip/>)
+    trip.setState({trips: [[L.latLng(0,0)], [L.latLng(0,0)], [L.latLng(0,0)]]})
+    expect(trip.state().trips.length).toEqual(3);
+    trip.instance().removeATrip();
+    expect(trip.state().trips.length).toEqual(2)
+}
+
+test("TestRemoveATrip", testRemoveATrip)
+
+function testResetTripPlaces(){
+    const trip = shallow(<Trip/>)
+    trip.instance().resetTripPlaces();
+    expect(trip.state().trips).toEqual([])
+    expect(trip.state().tripPlaces).toEqual([])
+    trip.setState({tripPlaces: [L.latLng(0,0)]})
+    trip.instance().resetTripPlaces();
+    expect(trip.state().tripPlaces).toEqual([])
+    expect(trip.state().trips).toEqual([])
+    trip.setState({trips: [[L.latLng(0,0), L.latLng(0,0)]]})
+    trip.instance().resetTripPlaces();
+    expect(trip.state().trips.length).toEqual(1)
+}
+
+test("TestResetTripPlaces", testResetTripPlaces)
