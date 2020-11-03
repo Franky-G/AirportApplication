60d7a02a2fbc54d53d291467a14d78eac8880a3a
Jacob Barth <jakebart@cs.colostate.edu>
2020-11-03 12:18:49 -0700

    Reverse method added
    Signed-off-by: Jacob Barth <jakebart@cs.colostate.edu>

--- server/src/main/java/com/tco/misc/Optimization.java
+++ server/src/main/java/com/tco/misc/Optimization.java
@@ -22,4 +22,15 @@ public class Optimization {
         return places;
     }
 
+    private static void TwoOptReverse(Map<String, String>[] places, int i1, int k) {
+        while(i1 < k) {
+            Map<String, String> temp = places[i1];
+            places[i1] = places[k];
+            places[k] = temp;
+            i1++;
+            k--;
+        }
+    }
+
+
 }
