420e219515a6fefb36693ef021bd373977360155
Jacob Barth <jakebart@cs.colostate.edu>
2020-11-03 17:22:18 -0700

    2opt algorithm code added
    Signed-off-by: Jacob Barth <jakebart@cs.colostate.edu>

--- server/src/main/java/com/tco/misc/Optimization.java
+++ server/src/main/java/com/tco/misc/Optimization.java
@@ -32,5 +32,36 @@ public class Optimization {
         }
     }
 
+    public static Map<String, String>[] TwoOpt(Map<String, String>[] places, Map<String, String> options) {
+        Integer tour[] = new Integer[places.length];
+        Boolean unvisited[] = new Boolean[places.length];
+        Integer distances[][] = new Integer[places.length][places.length];
+        for (int i = 0; i < places.length; ++i) {
+            for (int j = i; j < places.length; ++j) {
+                distances[i][j] = Math.toIntExact(CalculateDistance.ComputeDistance(places[i], places[j], Double.valueOf(options.get("earthRadius"))));
+                distances[j][i] = distances[i][j];
+            }
+        }
+        for (int i = 0; i < places.length; ++i) {
+            tour[i] = i;
+            unvisited[i] = true;
+        }
+
+        boolean improvement = true;
+        while (improvement) {
+            improvement = false;
+            for (int i = 0; i <= places.length-3; i++) {
+                for (int k = i+2; k <= places.length-1; k++) {
+                    Integer distDelta = (distances[i][k] + distances[i+1][k+1])-(distances[i+1][i]+distances[k][k+1]);
+                    if (distDelta < 0) {
+                        TwoOptReverse(places, i+1, k);
+                        improvement = true;
+                    }
+                }
+            }
+        }
+
+        return places;
+    }
 
 }
