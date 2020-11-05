b6e9eb70129e520b767db42f93785c2fae8d05bb
Jacob Barth <jakebart@cs.colostate.edu>
2020-11-04 19:25:28 -0700

    use of helper methods in 2opt
    Signed-off-by: Jacob Barth <jakebart@cs.colostate.edu>

--- server/src/main/java/com/tco/misc/Optimization.java
+++ server/src/main/java/com/tco/misc/Optimization.java
@@ -106,7 +106,7 @@ public class Optimization {
         return this.distances;
     }
 
-    private static void TwoOptReverse(Map<String, String>[] places, int i1, int k) {
+    private void TwoOptReverse(Map<String, String>[] places, int i1, int k) {
         while(i1 < k) {
             Map<String, String> temp = places[i1];
             places[i1] = places[k];
@@ -116,20 +116,8 @@ public class Optimization {
         }
     }
 
-    public static Map<String, String>[] TwoOpt(Map<String, String>[] places, Map<String, String> options) {
-        Integer tour[] = new Integer[places.length];
-        Boolean unvisited[] = new Boolean[places.length];
-        Integer distances[][] = new Integer[places.length][places.length];
-        for (int i = 0; i < places.length; ++i) {
-            for (int j = i; j < places.length; ++j) {
-                distances[i][j] = Math.toIntExact(CalculateDistance.ComputeDistance(places[i], places[j], Double.valueOf(options.get("earthRadius"))));
-                distances[j][i] = distances[i][j];
-            }
-        }
-        for (int i = 0; i < places.length; ++i) {
-            tour[i] = i;
-            unvisited[i] = true;
-        }
+    public Map<String, String>[] TwoOpt(Map<String, String>[] places, Map<String, String> options) {
+        Integer distances[][] = this.getDistances();
 
         boolean improvement = true;
         while (improvement) {
