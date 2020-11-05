13c3a1f24385f1d0e608d9c00ea02838ccb65d20
crepetown <kylecummings43@gmail.com>
2020-11-04 18:21:34 -0700

    Testing NN

--- server/src/test/java/com/tco/misc/TestOptimization.java
+++ server/src/test/java/com/tco/misc/TestOptimization.java
@@ -101,6 +101,17 @@ public class TestOptimization {
         assertEquals(Arrays.deepToString(expectedTour),Arrays.deepToString(tour));
     }
 
+    @Test
+    public void testContains(){
+        Boolean expected;
+        Boolean expected1;
+        Boolean[] contains = {false, false, false};
+        expected = optPlaces1.contains(contains, false);
+        expected1 = optPlaces1.contains(contains, true);
+        assertEquals(true, expected);
+        assertEquals(false, expected1);
+    }
+
 //    @Test
 //    public void testTotalDistancePlaces(){
 //        Integer[] tour = {0,1,2};
