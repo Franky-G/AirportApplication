0218f3fe3e7559acaad46e1a7c4f51d0ba535496
Frank Gansukh <frankyg@rams.colostate.edu>
2020-11-10 03:23:22 -0700

    code climate #2

--- server/src/main/java/com/tco/misc/ProcessFindRequest.java
+++ server/src/main/java/com/tco/misc/ProcessFindRequest.java
@@ -42,9 +42,7 @@ public class ProcessFindRequest {
     }
 
     public static String checkMatch(String match){
-        if (match == null || match.equals("78LuckyBoy78")){
-            return "";
-        }
+        if (match == null || match.equals("78LuckyBoy78")){ return ""; }
         String temp = "";
         for (int i=0; i<match.length(); i++){
             temp = checkMatchHelper(temp, match.charAt(i));
@@ -58,30 +56,19 @@ public class ProcessFindRequest {
     }
 
     public static void filterQUERYHelper(int limit){
-        if (limit > 0){
-            QUERY += " ASC LIMIT " + limit;
-        }
+        if (limit > 0){ QUERY += " ASC LIMIT " + limit; }
     }
 
     public static void filterQUERY(String match, int limit, Map<String, String[]> narrow) {
-
-        if (narrow.isEmpty()) {
-            narrowEmpty(match, limit);
-        }
-        else {
-            narrowHas(match, limit, narrow);
-        }
+        if (narrow.isEmpty()) { narrowEmpty(match, limit); }
+        else { narrowHas(match, limit, narrow); }
     }
 
     public static void narrowEmpty(String match, int limit){
         String temp = " WHERE country.name LIKE '%" + match + "%' OR region.name LIKE '%" + match + "%' OR world.name LIKE '%" + match + "%' OR world.municipality LIKE '%" + match + "%') AS tbl";
         QUERY1 += temp + helper2;
-
-        if (match.isEmpty() && limit == 0) {
-            QUERY += ") AS tbl ORDER BY RAND() LIMIT 1";
-        } else if (match.isEmpty()) {
-            QUERY += ") AS tbl ORDER BY RAND() LIMIT " + limit;
-        }
+        if (match.isEmpty() && limit == 0) { QUERY += ") AS tbl ORDER BY RAND() LIMIT 1"; }
+        else if (match.isEmpty()) { QUERY += ") AS tbl ORDER BY RAND() LIMIT " + limit; }
         else{
             QUERY += temp + helper2;
             filterQUERYHelper(limit);
@@ -91,40 +78,31 @@ public class ProcessFindRequest {
 
     public static void narrowHas(String match, int limit, Map<String,String[]> narrow){
         String temp = " WHERE country.name LIKE '%" + match + "%' OR region.name LIKE '%" + match + "%' OR world.name LIKE '%" + match + "%' OR world.municipality LIKE '%" + match + "%') AS tbl";
+        String typer = typeBuilder(getList(narrow.get("type")));
+        String wherer = getList(narrow.get("where")).stream().map(s -> "'" + s + "'").collect(Collectors.joining(", ", "(", ")"));
+        QUERY1 += temp;
+        narrowHasHelper(narrow, typer, wherer, temp);
+        filterQUERYHelper(limit);
+    }
+
+    public static void narrowHasHelper(Map<String,String[]> narrow, String typer, String wherer, String temp){
         List<String> types = getList(narrow.get("type"));
         List<String> wheres = getList(narrow.get("where"));
-        String typer = typeBuilder(types);
-        String wherer = wheres.stream().map(s -> "'" + s + "'").collect(Collectors.joining(", ", "(", ")"));
-        QUERY1 += temp;
-
-        if (narrow.get("type") != null && narrow.get("where") == null) { // Only Type specified
-            onlyType(types, typer, temp);
-        }
-        else if (narrow.get("where") != null && narrow.get("type") == null){ // Only Where specified
-            onlyWhere(wherer, temp);
-        }
-        else if (wheres.size() > 0 && types.size() > 0){ // Both Specified
-            hasBoth(types, wherer, typer, temp);
-        }
-        filterQUERYHelper(limit);
+        if (narrow.get("type") != null && narrow.get("where") == null) { onlyType(types, typer, temp); } // Only Type Specified
+        else if (narrow.get("where") != null && narrow.get("type") == null){ onlyWhere(wherer, temp); } // Only Where Specified
+        else if (wheres.size() > 0 && types.size() > 0){ hasBoth(types, wherer, typer, temp); } // Both Specified
     }
 
     public static List<String> getList(String[] arr){
         List<String> temp;
-        if (arr != null){
-            temp = Arrays.asList(arr);
-        }
-        else{
-            temp = Collections.singletonList("");
-        }
+        if (arr != null){ temp = Arrays.asList(arr); }
+        else{ temp = Collections.singletonList(""); }
         return temp;
     }
 
     public static String typeBuilder(List<String> types) {
         return types.stream().map(s -> {
-            if (s.equals("airport")){
-                return "'small_airport', 'medium_airport', 'large_airport'";
-            }
+            if (s.equals("airport")){ return "'small_airport', 'medium_airport', 'large_airport'"; }
             return "'" + s + "'";
         }).collect(Collectors.joining(", ", "(", ")"));
     }
@@ -145,7 +123,7 @@ public class ProcessFindRequest {
 
     public static void hasBoth(List<String> types, String wherer, String typer, String temp){
         if (types.contains("balloonport") || types.contains("heliport") || types.contains("airport")) {
-            String temp1 = " WHERE (tbl.country IN " + wherer + " OR tbl.region IN " + wherer + " OR tbl.municipality IN " + wherer + ") AND (tbl.type IN " + typer + ")";
+            String temp1 = " WHERE (tbl.type IN " + typer + ") AND (tbl.country IN " + wherer + " OR tbl.region IN " + wherer + " OR tbl.municipality IN " + wherer + ")";
             QUERY += temp + temp1 + "ORDER BY tbl.name";
             QUERY1 += temp1 + "ORDER BY tbl.name";
         }
@@ -168,7 +146,6 @@ public class ProcessFindRequest {
         Connection con = DriverManager.getConnection(db_url, db_user, db_pass);
         Statement query = con.createStatement();
         ResultSet result = query.executeQuery(QUERY);
-
         while (result.next() && counter < 500){
             allLocations.add(processPlacesHelper(result));
             counter++;
@@ -188,42 +165,28 @@ public class ProcessFindRequest {
         location.put("country", result.getString("country"));
 
         String idString = location.get("id");
-        if (idString != null){
-            location.put("url", "https://www.aopa.org/destinations/airports/" + idString + "/details");
-        }
+        if (idString != null){ location.put("url", "https://www.aopa.org/destinations/airports/" + idString + "/details"); }
         return location;
     }
 
     public static int processFound(String match, int limit){
         matcher = checkMatch(match);
         LinkedHashMap<String,String> foundMap = new LinkedHashMap<>();
-
         try{
             Connection con = DriverManager.getConnection(db_url, db_user, db_pass);
             Statement query = con.createStatement();
             ResultSet result = query.executeQuery(QUERY1);
-
-            while (result.next()){
-                foundMap.put("found", result.getString("found"));
-            }
-        } catch (Exception e) {
-            System.err.println("Exception: Can't Connect To Data Base: " + e.getMessage());
+            while (result.next()){ foundMap.put("found", result.getString("found")); }
         }
-
+        catch (Exception e) { System.err.println("Exception: Can't Connect To Data Base: " + e.getMessage()); }
         int found = Integer.parseInt(foundMap.get("found"));
         return foundReturn(matcher, limit, found);
     }
 
     public static int foundReturn(String match, int limit, int found){
-        if (match.isEmpty() && limit == 0){
-            return 1;
-        }
-        if (match.isEmpty()){
-            return limit;
-        }
-        else{
-            return found;
-        }
+        if (match.isEmpty() && limit == 0){ return 1; }
+        if (match.isEmpty()){ return limit; }
+        else{ return found; }
     }
 
 
@@ -245,21 +208,17 @@ public class ProcessFindRequest {
             getWhereHelper(temp, result);
             getWhereHelper(temp, result1);
             getWhereHelper(temp, result2);
-
             where = new String[temp.size()];
             where = temp.toArray(where);
             return where;
-        } catch (Exception e) {
-            System.err.println("Exception: Can't Connect To Data Base.");
         }
+        catch (Exception e) { System.err.println("Exception: Can't Connect To Data Base."); }
         return where;
     }
 
     public static void getWhereHelper(List<String> temp, ResultSet result) throws SQLException {
         while (result.next()){
-            if (result.getString("name") != null) {
-                temp.add(result.getString("name"));
-            }
+            if (result.getString("name") != null) { temp.add(result.getString("name")); }
         }
     }
 }
